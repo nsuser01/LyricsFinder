a8f56a0c22891adc10a3789905223ec18d6ce431
Narendra Sharma <narendrasharmasabalpura@gmail.com>
2021-12-23 11:27:39 +0530

    Updates

---
+++ precache-manifest.e7893c6cf085eaf901b5282021d9c22e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab8421ca13e04a3e9c353dedfc2b3bf5",
+    "url": "/LyricsFinder/index.html"
+  },
+  {
+    "revision": "cfd932322c61edf0e54e",
+    "url": "/LyricsFinder/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "ac6ba3e676f69491a916",
+    "url": "/LyricsFinder/static/js/2.0462ecda.chunk.js"
+  },
+  {
+    "revision": "cfd932322c61edf0e54e",
+    "url": "/LyricsFinder/static/js/main.53b119ee.chunk.js"
+  },
+  {
+    "revision": "7c1eb1db8ca358548c2b",
+    "url": "/LyricsFinder/static/js/runtime~main.fca0ecc5.js"
+  }
+]);
